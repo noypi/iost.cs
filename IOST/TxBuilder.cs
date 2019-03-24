da4d74b792f864bd3a6cf9d7cbac61fc023f2e96
saifuddin <adrian.migraso@gmail.com>
2019-03-24 18:33:32 +0800

    add nuget

--- IOST/TxBuilder.cs
+++ IOST/TxBuilder.cs
@@ -14,12 +14,12 @@ namespace IOST
         }
 
         /// <summary>
-        /// 
+        /// Transfer a token to an account
         /// </summary>
-        /// <param name="token"></param>
-        /// <param name="from"></param>
-        /// <param name="to"></param>
-        /// <param name="amount"></param>
+        /// <param name="token">the token to transfer, example: "iost"</param>
+        /// <param name="from">the source account name</param>
+        /// <param name="to">the destination account name</param>
+        /// <param name="amount">the amount to transfer</param>
         /// <param name="memo">maximum of 512 characters</param>
         /// <returns></returns>
         public Transaction Transfer(string token, string from, string to, double amount, string memo)
@@ -32,8 +32,18 @@ namespace IOST
             return tx;
         }
 
+        /// <summary>
+        /// Creates a new account
+        /// </summary>
+        /// <param name="name">the account</param>
+        /// <param name="creator">the one who pays for the new account</param>
+        /// <param name="ownerkey">the public key with the 'owner' permission</param>
+        /// <param name="activekey">the public key with the 'active' permission</param>
+        /// <param name="initialGasPledge"></param>
+        /// <param name="initialRAM"></param>
+        /// <returns></returns>
         public Transaction NewAccount(string name, string creator, string ownerkey, string activekey,
-                                        int initialRAM, double initialGasPledge)
+                                      double initialGasPledge = 1000000, int initialRAM = 1024)
         {
             if (!ValidatePubKey(ownerkey) || !ValidatePubKey(activekey))
             {
--- README.md
+++ README.md
@@ -4,8 +4,11 @@ Support .Net Standard 2.0+ / .Net Framework 4.5+
 ## Features
 - Uses a Secure Keychain to keep the private key secured
 
-## Limitations
-For .Net Framework 4.5+, must define the following functions if Secp256k1 if needed
+## Buy me a pizza
+Thank you, my IOST account is: noypi
+
+## Limitations (for now)
+For .Net Framework 4.5+, must define the following functions if Secp256k1 is needed
 - IOST.CryptoSignSecp256k1
 - IOST.CryptoGetPubkeySecp256k1
 - IOST.CryptoGetPubkeySecp256k1Compressed
