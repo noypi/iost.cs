b6f10b8a3a8162cd6bc9d4272f397fc240be6b95
saifuddin <adrian.migraso@gmail.com>
2019-03-26 18:54:52 +0800

    done #10

--- IOST/Channels.cs
+++ IOST/Channels.cs
@@ -4,13 +4,15 @@ namespace IOSTSdk
 
     internal class Channels
     {
-        internal static Channel NewAustrialia() => new Channel(Servers.Australia, ChannelCredentials.Insecure);
-        internal static Channel NewCanada() => new Channel(Servers.Canada, ChannelCredentials.Insecure);
-        internal static Channel NewFrance() => new Channel(Servers.France, ChannelCredentials.Insecure);
-        internal static Channel NewGermany() => new Channel(Servers.Germany, ChannelCredentials.Insecure);
-        internal static Channel NewJapan() => new Channel(Servers.Japan, ChannelCredentials.Insecure);
-        internal static Channel NewKorea() => new Channel(Servers.Korea, ChannelCredentials.Insecure);
-        internal static Channel NewUK() => new Channel(Servers.UK, ChannelCredentials.Insecure);
-        internal static Channel NewUS() => new Channel(Servers.US, ChannelCredentials.Insecure);
+        internal static Channel NewAustrialia() => NewServer(Servers.Australia);
+        internal static Channel NewCanada() => NewServer(Servers.Canada);
+        internal static Channel NewFrance() => NewServer(Servers.France);
+        internal static Channel NewGermany() => NewServer(Servers.Germany);
+        internal static Channel NewJapan() => NewServer(Servers.Japan);
+        internal static Channel NewKorea() => NewServer(Servers.Korea);
+        internal static Channel NewUK() => NewServer(Servers.UK);
+        internal static Channel NewUS() => NewServer(Servers.US);
+
+        internal static Channel NewServer(string loc) => new Channel(loc, ChannelCredentials.Insecure);
     }
 }
